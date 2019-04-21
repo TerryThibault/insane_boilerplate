729923028e5a53f9ae7941625aa1fe665e416907
Terry Thibault <terrythibault@Terrys-MBP.attlocal.net>
2019-04-20 20:20:00 -0400

    remove extra functions

--- password-brute-force.py
+++ password-brute-force.py
@@ -5,24 +5,6 @@ import pickle
 from multiprocessing.dummy import Pool as ThreadPool 
 import requests
 
-## Make an object
-class object:
-    def __init__(self, param):
-        self.param = param
-
-## Saving & loading objects to/from disk
-def save_object(obj, filename):
-    with open(filename, 'wb') as output:
-        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
-
-def load_object(obj, filename):
-    if os.path.isfile(filename):
-        with open(filename, 'rb') as input:
-            obj = pickle.load(input)
-
-## Do whatever in parallel. Come back to this later with better syntax.
-## It works, but doesn't limit the number of threads spun up. 
-## Also doesn't handle blocking functions too well.
 def thread_starter():
     with open('password-list.txt', 'r') as f:
         passwords = f.read().splitlines()
